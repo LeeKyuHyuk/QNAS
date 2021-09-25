19c077e6c3eebad5687cca3a8942f190e8a55a5a
KyuHyuk Lee <lee@kyuhyuk.kr>
2021-09-25 23:04:13 +0900

    Nginx, MariaDB 추가

---
+++ config.mk
@@ -0,0 +1,8 @@
+export CONFIG_QNAS_VERSION = 1.0.0
+export CONFIG_TARGET = x86_64-qnas-linux-musl
+export CONFIG_LINUX_ARCH = x86_64
+export CONFIG_GCC_ARCH = x86-64
+export CONFIG_STRIP_AND_DELETE_DOCS = 1
+export CONFIG_HOSTNAME = qnas
+export CONFIG_ROOT_PASSWD = qnas
+export CONFIG_LOCAL_TIMEZONE=Asia/Seoul
